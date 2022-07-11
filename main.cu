d6d9a491b84f54b6a1d884d520c78ffbb1e46db4
Pavel Akhtyamov <akhtyamovpavel@gmail.com>
2022-07-11 19:35:20 +0300

    Dev version to deploy on cluster

--- main.cu
+++ main.cu
@@ -22,23 +22,50 @@ __global__ void Rkendall_gpu_atomic(const double* col1, const double* col2, cons
   }
 }
 
-__global__ void Rkendall_gpu_atomic_float(const float* col1, const float* col2, const int n, const int m, unsigned long long* R){
+__global__ void Rkendall_gpu_atomic_float(float* array, const int n, const int m, unsigned int* result) {
+  
   int row1 = blockIdx.y * blockDim.y + threadIdx.y;
   int row2 = blockIdx.x * blockDim.x + threadIdx.x;
-  if (row1 < row2 && row2 < n){
-    if ((col1[row1] - col1[row2]) * (col2[row1] - col2[row2]) < 0){
-      atomicAdd(R, 1);
-    }
+
+
+  for (int col1_num = 0; col1_num < m; ++col1_num) {
+      for (int col2_num = col1_num + 1; col2_num < m; ++col2_num) {
+          float* col1 = array + n * col1_num;
+          float* col2 = array + n * col2_num;
+
+          if (row1 < row2 && row2 < n){
+            if ((col1[row1] - col1[row2]) * (col2[row1] - col2[row2]) < 0){
+              atomicAdd(result + col1_num * m + col2_num, 1);
+              atomicAdd(result + col2_num * m + col1_num, 1);
+            }
+          }
+      }
   }
 }
 
 
+__global__ void Rkendall_gpu_atomic_float_different_blocks(float* array, float* array2, const int n, const int m, const int m_b, unsigned int* result) {
+  
+  int row1 = blockIdx.y * blockDim.y + threadIdx.y;
+  int row2 = blockIdx.x * blockDim.x + threadIdx.x;
+
+
+  for (int col1_num = 0; col1_num < m; ++col1_num) {
+      for (int col2_num = 0; col2_num < m_b; ++col2_num) {
+          float* col1 = array + n * col1_num;
+          float* col2 = array2 + n * col2_num;
+
+          if (row1 < row2 && row2 < n){
+            if ((col1[row1] - col1[row2]) * (col2[row1] - col2[row2]) < 0){
+              atomicAdd(result + col2_num * m + col1_num, 1);
+            }
+          }
+      }
+  }
+}
+
 extern "C" void matrix_Kendall_distance_same_block(double* a, double * b /* not used */, double* c, int* n, int* m, int* m_b){
 
-  size_t dataset_column_size = *n * sizeof(double);
-  size_t dataset_float_size = *n * sizeof(float);
-  
-  size_t reverse_max_size = sizeof(unsigned long long);
 
   int array_size = *n * *m;
 
@@ -54,80 +81,82 @@ extern "C" void matrix_Kendall_distance_same_block(double* a, double * b /* not 
 
   cudaMemcpy(d_array, array_new, array_size * sizeof(float), cudaMemcpyHostToDevice);
 
-  unsigned long long* device_R;
-  cudaMalloc(&device_R, reverse_max_size);
+  int threads = 16;
+  int blocks_in_row = (*n + threads - 1) / threads;
+  int blocks_in_col = (*n + threads - 1) / threads;
 
-  cudaMemset(device_R, 0, reverse_max_size);
-  unsigned long long host_R = 0;
-  for (int col1 = 0; col1 < *m; col1++){
-    float* first_column_device_ptr = d_array + col1 * *n;
-    //cudaMalloc(&first_column_device_ptr, dataset_float_size);
-    //cudaMemcpy(first_column_device_ptr, array_new + col1 * *n, dataset_float_size, cudaMemcpyHostToDevice);
-    for (int col2 = col1 + 1; col2 < *m; col2 ++){
-      float* second_column_device_ptr = d_array + col2 * *n;
-      // std::cout << col1 << " " << col2 << std::endl;
-      //cudaMalloc(&second_column_device_ptr, dataset_float_size);
-      //cudaMemcpy(second_column_device_ptr, array_new + col2 * *n, dataset_float_size, cudaMemcpyHostToDevice);
-      cudaMemset(device_R, 0, reverse_max_size);
-      cudaDeviceSynchronize(); 
-      int threads = 16;
-      int blocks_in_row = (*n + threads - 1) / threads;
-      int blocks_in_col = (*n + threads - 1) / threads;
+  dim3 THREADS(threads, threads);
+  dim3 BLOCKS(blocks_in_row, blocks_in_col);
 
-      dim3 THREADS(threads, threads);
-      dim3 BLOCKS(blocks_in_row, blocks_in_col);
+  unsigned int* d_result;
+  unsigned int* h_result = new unsigned int[(*m) * (*m)];
+  cudaMalloc(&d_result, (*m) * (*m) * sizeof(unsigned int));
+  cudaMemset(d_result, 0, (*m) * (*m) * sizeof(unsigned int));
 
-      Rkendall_gpu_atomic_float<<<BLOCKS, THREADS>>>(first_column_device_ptr, second_column_device_ptr, *n, *m, device_R);
-      // gpuErrchk( cudaPeekAtLastError() );
-      cudaMemcpy(&host_R, device_R, reverse_max_size, cudaMemcpyDeviceToHost);
-      // std::cout << host_R << std::endl;
-      c[col1 * *m + col2] = host_R * 2.0f / *n / (*n - 1);
-      c[col2 * *m + col1] = c[col1 * *m + col2];
+  Rkendall_gpu_atomic_float<<<BLOCKS, THREADS>>>(d_array, *n, *m, d_result);
 
-      // cudaFree(second_column_device_ptr);
-      // cudaFree(device_R);
-    }
-    // cudaFree(first_column_device_ptr);
+  cudaMemcpy(h_result, d_result, (*m) * (*m) * sizeof(float), cudaMemcpyDeviceToHost);
+
+
+  for (int i = 0; i < (*m) * (*m); ++i) {
+    c[i] = h_result[i] * 2.0f / (*n) / (*n - 1);
   }
+
+  free(h_result);
+  cudaFree(d_result);
   cudaFree(d_array);
 }
 
 extern "C" void matrix_Kendall_distance_different_blocks(double* a, double* b, double* c, int* n, int* m, int* m_b){
 
-  size_t dataset_column_size = *n * sizeof(double);
-  size_t reverse_max_size = sizeof(unsigned long long);
-  for (int col1 = 0; col1 < *m; col1++){
-    double* first_column_device_ptr;
-    cudaMalloc(&first_column_device_ptr, dataset_column_size);
-    cudaMemcpy(first_column_device_ptr, a + col1 * *n, dataset_column_size, cudaMemcpyHostToDevice);
-    // std::cout << col1 << std::endl;
-    for (int col2 = 0; col2 < *m_b; col2 ++){
-      double* second_column_device_ptr;
-      
-      cudaMalloc(&second_column_device_ptr, dataset_column_size);
-      cudaMemcpy(second_column_device_ptr, b + col2 * *n, dataset_column_size, cudaMemcpyHostToDevice);
-      unsigned long long host_R = 0;
-      unsigned long long* device_R;
-      cudaMalloc(&device_R, reverse_max_size);
-      cudaMemcpy(device_R, &host_R, reverse_max_size, cudaMemcpyHostToDevice);
-      int threads = 16;
-      int blocks_in_row = (*n + threads - 1) / threads;
-      int blocks_in_col = (*n + threads - 1) / threads;
+  int array_size = *n * *m;
+  float* array_new = new float[*n * *m];
 
-      dim3 THREADS(threads, threads);
-      dim3 BLOCKS(blocks_in_row, blocks_in_col);
-
-      Rkendall_gpu_atomic<<<BLOCKS, THREADS>>>(first_column_device_ptr, second_column_device_ptr, *n, *m, device_R);
-
-      cudaMemcpy(&host_R, device_R, reverse_max_size, cudaMemcpyDeviceToHost);
-      c[col2 * *m + col1] = host_R * 2.0f / *n / (*n - 1);
-      // c[col2 * *m + col1] = c[col1 * *m + col2];
-
-      cudaFree(second_column_device_ptr);
-      cudaFree(device_R);
-    }
-    cudaFree(first_column_device_ptr);
+  for (int i = 0; i < array_size; ++i) {
+    array_new[i] = a[i];
   }
+
+  int array2_size = *n * (*m_b);
+  float* array2_new = new float[array2_size];
+
+  for (int i = 0; i < array2_size; ++i) {
+    array2_new[i] = b[i];
+  }
+
+  float* d_array;
+  float* d_array2;
+
+  cudaMalloc(&d_array, array_size * sizeof(float));
+  cudaMalloc(&d_array2, array_size * sizeof(float));
+
+  cudaMemcpy(d_array, array_new, array_size * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_array2, array2_new, array2_size * sizeof(float), cudaMemcpyHostToDevice);
+
+  int threads = 16;
+  int blocks_in_row = (*n + threads - 1) / threads;
+  int blocks_in_col = (*n + threads - 1) / threads;
+
+  dim3 THREADS(threads, threads);
+  dim3 BLOCKS(blocks_in_row, blocks_in_col);
+
+  unsigned int* d_result;
+  unsigned int* h_result = new unsigned int[(*m) * (*m_b)];
+  cudaMalloc(&d_result, (*m) * (*m_b) * sizeof(unsigned int));
+  cudaMemset(d_result, 0, (*m) * (*m_b) * sizeof(unsigned int));
+
+  Rkendall_gpu_atomic_float_different_blocks<<<BLOCKS, THREADS>>>(d_array, d_array2, *n, *m, *m_b, d_result);
+
+  cudaMemcpy(h_result, d_result, (*m) * (*m_b) * sizeof(float), cudaMemcpyDeviceToHost);
+
+
+  for (int i = 0; i < (*m) * (*m_b); ++i) {
+    c[i] = h_result[i] * 2.0f / (*n) / (*n - 1);
+  }
+
+  free(h_result);
+  cudaFree(d_result);
+  cudaFree(d_array);
+  cudaFree(d_array2);
 }
 
 
--- test.R
+++ test.R
@@ -1,14 +1,43 @@
 dyn.load("mtrx.so")
 source("mtrx.R")
+library(amap)
+library(edgeR)
+library(biomaRt)
+library(Hobotnica)
+library(MASS)
 
 args = commandArgs(trailingOnly=TRUE)
 datain = args[1]
+method = args[2]
+times = strtoi(args[3])
+
 dataout = "Kendall_2.csv"
 print('Reading table')
 data <- t(as.matrix(read.table(datain, header=T, row.names = 1, sep=",")))
 print('Completed reading')
-st_t = as.numeric(Sys.time()) * 1000000
-distMatrix_mtrx <- mtrx_Kendall_distance(data, batch_size = 10000)
-print(distMatrix_mtrx[1:10, 1:10])
-print(as.numeric(Sys.time()) * 1000000 - st_t)
+
+measurements <- numeric(times)
+
+for (i in 1:times) {
+    st_t <- as.numeric(Sys.time()) * 1000000
+#data_matrix <- as.matrix(data)
+
+    if (method == 'GPU') {
+        distMatrix_mtrx <- mtrx_Kendall_distance(data, batch_size = 10000)
+    } else {
+        #print('Calc dist')
+        distMatrix_mtrx <- Dist(t(data), method="kendall", nbproc=24)
+    }
+    end_time <- as.numeric(Sys.time()) * 1000000
+
+    measurements[i] <- end_time - st_t
+    #print(as.numeric(Sys.time()) * 1000000 - st_t)
+}
+
+print(mean(as.matrix(measurements)))
+print(sd(as.matrix(measurements)))
+print(as.matrix(measurements))
+print('Matrix')
+#write.table(distMatrix_mtrx, 'matrix.csv', sep=',')
+#print(distMatrix_mtrx[1:10, 1:10])
 cat('\n', file = dataout, append = TRUE)
